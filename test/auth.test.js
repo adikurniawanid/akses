ec67473a23f666a101b1befe3781383cc906c61c
Adi Kurniawan <adikurniawan.dev@gmail.com>
2023-07-21 11:42:44 +0700

    feat: integration test
    + validation input login
    + invalid email or password
    + new account google
    + login with google
    + new account facebook
    + login with facebook

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,19 +1,23 @@
 const supertest = require("supertest");
 const app = require("../app");
 const removeTestUser = require("./utils/removeUserTest.util");
+const APITestVersion = "v1";
+const googleIdToken = process.env.TEST_GOOGLE_ID_TOKEN;
+const facebookIdToken = process.env.TEST_FACEBOOK_ID_TOKEN;
+
+let googleUsernameTestTest = "";
+let facebookUsernameTestTest = "";
 
 describe("akses Testing", () => {
-  afterAll(async () => {
-    await removeTestUser("usertesting");
-  });
-
-  it("Register User", async () => {
-    const result = await supertest(app).post("/register").send({
-      name: "User Testing",
-      email: "usertesting@mail.com",
-      password: "usertesting@mail.com",
-      username: "usertesting",
-    });
+  it("201 > Register User", async () => {
+    const result = await supertest(app)
+      .post(`/${APITestVersion}/register`)
+      .send({
+        name: "User Testing",
+        email: "usertesting@mail.com",
+        password: "usertesting@mail.com",
+        username: "usertesting",
+      });
 
     expect(result.status).toBe(201);
     expect(result.body.message).toBe("User created successfully");
@@ -25,8 +29,25 @@ describe("akses Testing", () => {
     expect(result.body.token.refreshToken).toBeDefined();
   });
 
-  it("Login", async () => {
-    const result = await supertest(app).post("/login").send({
+  it("400 > Validation Input", async () => {
+    const result = await supertest(app)
+      .post(`/${APITestVersion}/register`)
+      .send({
+        name: "",
+        email: "usertesting@mail.com",
+        password: "",
+        username: "usertesting",
+      });
+
+    expect(result.status).toBe(400);
+    expect(result.body.message).toBe(
+      "Validation failed, entered data is incorrect."
+    );
+    expect(result.body.errors).toBeDefined();
+  });
+
+  it("200 > Login", async () => {
+    const result = await supertest(app).post(`/${APITestVersion}/login`).send({
       email: "usertesting@mail.com",
       password: "usertesting@mail.com",
     });
@@ -39,5 +60,99 @@ describe("akses Testing", () => {
     expect(result.body.data.avatarUrl).toBeNull();
     expect(result.body.token.accessToken).toBeDefined();
     expect(result.body.token.refreshToken).toBeDefined();
+  });
+
+  it("401 > Invalid Email or Password", async () => {
+    const result = await supertest(app).post(`/${APITestVersion}/login`).send({
+      email: "usertesting@mail.coma",
+      password: "usertesting@mail.coma",
+    });
+
+    expect(result.status).toBe(401);
+    expect(result.body.message).toBe("Invalid email or password");
+  });
+
+  it("201 > Create New Account With Google", async () => {
+    const result = await supertest(app)
+      .post(`/${APITestVersion}/login-with-google`)
+      .send({
+        googleIdToken,
+      });
+
+    expect(result.status).toBe(201);
+    expect(result.body.message).toBe("User created successfully");
+    expect(result.body.data.publicId).toBeDefined();
+    expect(result.body.data.name).toBe("Adi Kurniawan");
+    expect(result.body.data.username).toBeDefined();
+    expect(result.body.data.avatarUrl).toBeDefined();
+    expect(result.body.token.accessToken).toBeDefined();
+    expect(result.body.token.refreshToken).toBeDefined();
+  });
+
+  it("200 > Login With Google", async () => {
+    const result = await supertest(app)
+      .post(`/${APITestVersion}/login-with-google`)
+      .send({
+        googleIdToken,
+      });
+
+    expect(result.status).toBe(200);
+    expect(result.body.message).toBe("Login sucessfully");
+    expect(result.body.data.publicId).toBeDefined();
+    expect(result.body.data.name).toBe("Adi Kurniawan");
+    expect(result.body.data.username).toBeDefined();
+    expect(result.body.data.avatarUrl).toBeDefined();
+    expect(result.body.token.accessToken).toBeDefined();
+    expect(result.body.token.refreshToken).toBeDefined();
+
+    googleUsernameTestTest = result.body.data.username;
+  });
+
+  it("201 > Create New Account With Facebook", async () => {
+    const result = await supertest(app)
+      .post(`/${APITestVersion}/login-with-facebook`)
+      .send({
+        facebookIdToken,
+      });
+
+    expect(result.status).toBe(201);
+    expect(result.body.message).toBe("User created successfully");
+    expect(result.body.data.publicId).toBeDefined();
+    expect(result.body.data.name).toBe("Adi Kurniawan");
+    expect(result.body.data.username).toBeDefined();
+    expect(result.body.data.avatarUrl).toBeDefined();
+    expect(result.body.token.accessToken).toBeDefined();
+    expect(result.body.token.refreshToken).toBeDefined();
+  });
+
+  it("200 > Login With Facebook", async () => {
+    const result = await supertest(app)
+      .post(`/${APITestVersion}/login-with-facebook`)
+      .send({
+        facebookIdToken,
+      });
+
+    expect(result.status).toBe(200);
+    expect(result.body.message).toBe("Login sucessfully");
+    expect(result.body.data.publicId).toBeDefined();
+    expect(result.body.data.name).toBe("Adi Kurniawan");
+    expect(result.body.data.username).toBeDefined();
+    expect(result.body.data.avatarUrl).toBeDefined();
+    expect(result.body.token.accessToken).toBeDefined();
+    expect(result.body.token.refreshToken).toBeDefined();
+
+    facebookUsernameTestTest = result.body.data.username;
+  });
+
+  afterAll(async () => {
+    await removeTestUser("usertesting");
+  });
+
+  afterAll(async () => {
+    await removeTestUser(googleUsernameTestTest);
+  });
+
+  afterAll(async () => {
+    await removeTestUser(facebookUsernameTestTest);
   });
 });
